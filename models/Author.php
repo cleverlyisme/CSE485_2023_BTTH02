cc337fe7b9fb9b18fcc9c4dc9800af08a33b257b
ToTungDuong <tod25504@gmail.com>
2023-03-07 22:59:55 +0700

    feat: code function Delete Author

--- models/Author.php
+++ models/Author.php
@@ -42,4 +42,9 @@ class Author {
         
        return $this->db->runSql($sql, $arguments);
     }
+    public function delete($arguments) {
+        $sql = "DELETE FROM tacgia WHERE ma_tgia=:ma_tgia;";
+
+       return $this->db->runSql($sql, $arguments);
+    }
 }
